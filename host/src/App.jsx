d1f6420c2a15949acab079c8a2f49dd2689ee2b2
dm.choi <dm.choi@logiall.com>
2023-11-10 00:44:03 +0900

    test

---
+++ host/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Button from "remoteApp/Button";
+
+function App() {
+  return (
+    <>
+      <h1>Host Application</h1>
+      <Button />
+    </>
+  );
+}
+
+export default App;
